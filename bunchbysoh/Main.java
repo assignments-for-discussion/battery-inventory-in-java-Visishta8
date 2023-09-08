a1b8f4aca3284b991165563cfadb543fb4997b0e
SRI VISISHTA PAPPALA <visishtapappala@gmail.com>
2023-09-08 19:28:38 +0530

    More precise test cases

--- bunchbysoh/Main.java
+++ bunchbysoh/Main.java
@@ -9,9 +9,10 @@ public class Main {
 
   static CountsBySoH countBatteriesByHealth(int[] presentCapacities) {
     CountsBySoH counts = new CountsBySoH();
+    int rated_capacity = 120;
     
     for (int capacity : presentCapacities) {
-        double soh = (capacity / 120.0) * 100; 
+        double soh = (capacity / rated_capacity) * 100; 
         if (soh > 80) {
             counts.healthy++;
         } else if (soh >= 65) {
@@ -28,6 +29,27 @@ public class Main {
     System.out.println("Counting batteries by SoH...\n");
     int[] presentCapacities = {115, 118, 80, 95, 91, 77};
     CountsBySoH counts = countBatteriesByHealth(presentCapacities);
+    // Test when SoH is exactly 80%
+    int[] capacities80 = {96}; // 80% of 120
+    CountsBySoH counts80 = countBatteriesByHealth(capacities80);
+    assert(counts80.healthy == 0);
+    assert(counts80.exchange == 1);
+    assert(counts80.failed == 0);
+
+    // Test when SoH is exactly 65%
+    int[] capacities65 = {78}; // 65% of 120
+    CountsBySoH counts65 = countBatteriesByHealth(capacities65);
+    assert(counts65.healthy == 0);
+    assert(counts65.exchange == 1);
+    assert(counts65.failed == 0);
+
+    // Test when present capacity is equal to rated capacity
+    int[] capacitiesEqual = {120};
+    CountsBySoH countsEqual = countBatteriesByHealth(capacitiesEqual);
+    assert(countsEqual.healthy == 1);
+    assert(countsEqual.exchange == 0);
+    assert(countsEqual.failed == 0);
+    
     assert(counts.healthy == 2);
     assert(counts.exchange == 3);
     assert(counts.failed == 1);
